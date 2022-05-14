a5ffe9a6456fe886d56aa83fc1f5706b1ac3025f
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-05-14 18:32:41 -0300

    Aula 04

---
+++ Aula04/src/Aula04.java
@@ -0,0 +1,8 @@
+public class Aula04 {
+    public static void main(String[] args) throws Exception {
+        Caneta c1 = new Caneta("BIC", "Amarela", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("TIC", "Laranja", 1.0f);
+        c2.status();
+    }
+}
