5e5604d03ad49f559074324e697a0db6546dd669
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-05-14 09:17:08 -0300

    Algumas correções

--- OlaMundo/OlaMundo.java
+++ OlaMundo/OlaMundo.java
@@ -1,5 +1,8 @@
+package OlaMundo;
+
 public class OlaMundo {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
     }
 }
