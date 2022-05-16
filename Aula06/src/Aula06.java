9b137ef7ff66df0c0c5ceead820e0e13e14ac0c6
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-05-16 13:40:47 -0300

    Aula 06

---
+++ Aula06/src/Aula06.java
@@ -0,0 +1,9 @@
+public class Aula06 {
+    public static void main(String[] args) throws Exception {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+    }
+}
